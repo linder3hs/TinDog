e41b8589cb0e728a0d76f0882666940110063b66
linder <linderhassinger00@gmail.com>
2018-03-24 18:06:35 -0500

    card translation

--- TinDog/Controller/HomeViewController.swift
+++ TinDog/Controller/HomeViewController.swift
@@ -16,28 +16,43 @@ class NavigationImageView :UIImageView {
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var homewrapper: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let titleView = NavigationImageView()
         titleView.image = UIImage(named:"Actions")
         self.navigationItem.titleView = titleView
+        
+        let homeGR = UIPanGestureRecognizer(target: self, action: #selector(cardDragged(gestureReconizer:)))
+        self.cardView.addGestureRecognizer(homeGR)
+        
     }
-
+    
+    @objc func cardDragged(gestureReconizer : UIPanGestureRecognizer) {
+        //print("Drag \(gestureReconizer.translation(in: view))")
+        let cardPoint = gestureReconizer.translation(in: view)
+        self.cardView.center = CGPoint(x: self.view.bounds.width / 2 + cardPoint.x, y: self.view.bounds.height / 2 + cardPoint.y)
+        
+        if gestureReconizer.state == .ended {
+            if self.cardView.center.x < (self.view.bounds.width / 2 - 100) {
+                print("Dislike")
+            }
+            
+            if self.cardView.center.x > (self.view.bounds.width / 2 + 100) {
+                print("Like")
+            }
+            
+            self.cardView.center = CGPoint(x: self.homewrapper.bounds.width / 2 , y: (self.homewrapper.bounds.height / 2) - 30)
+         }
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
